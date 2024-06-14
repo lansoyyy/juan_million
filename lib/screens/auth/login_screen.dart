5bc19ce57f5e6cf0bd83eb7ce75656c0f460a5cd
Lans12345 <olanalans12345@gmail.com>
2024-06-15 07:53:54 +0800

    added forgot password

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:juan_million/screens/auth/customer_signup_screen.dart';
 import 'package:juan_million/screens/auth/signup_screen.dart';
 import 'package:juan_million/screens/business_home_screen.dart';
@@ -72,6 +73,120 @@ class _LoginScreenState extends State<LoginScreen> {
               isRequred: false,
               controller: password,
               label: 'Password',
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 25),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: ((context) {
+                        final formKey = GlobalKey<FormState>();
+                        final TextEditingController emailController =
+                            TextEditingController();
+
+                        return AlertDialog(
+                          backgroundColor: Colors.grey[100],
+                          title: TextWidget(
+                            text: 'Forgot Password',
+                            fontSize: 14,
+                            color: Colors.black,
+                          ),
+                          content: Form(
+                            key: formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                TextFieldWidget(
+                                  hint: 'Email',
+                                  textCapitalization: TextCapitalization.none,
+                                  inputType: TextInputType.emailAddress,
+                                  label: 'Email',
+                                  controller: emailController,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter an email address';
+                                    }
+                                    final emailRegex = RegExp(
+                                        r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                                    if (!emailRegex.hasMatch(value)) {
+                                      return 'Please enter a valid email address';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: (() {
+                                Navigator.pop(context);
+                              }),
+                              child: TextWidget(
+                                text: 'Cancel',
+                                fontSize: 12,
+                                color: Colors.black,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: (() async {
+                                if (formKey.currentState!.validate()) {
+                                  try {
+                                    Navigator.pop(context);
+                                    await FirebaseAuth.instance
+                                        .sendPasswordResetEmail(
+                                            email: emailController.text);
+                                    showToast(
+                                        'Password reset link sent to ${emailController.text}');
+                                  } catch (e) {
+                                    String errorMessage = '';
+
+                                    if (e is FirebaseException) {
+                                      switch (e.code) {
+                                        case 'invalid-email':
+                                          errorMessage =
+                                              'The email address is invalid.';
+                                          break;
+                                        case 'user-not-found':
+                                          errorMessage =
+                                              'The user associated with the email address is not found.';
+                                          break;
+                                        default:
+                                          errorMessage =
+                                              'An error occurred while resetting the password.';
+                                      }
+                                    } else {
+                                      errorMessage =
+                                          'An error occurred while resetting the password.';
+                                    }
+
+                                    showToast(errorMessage);
+                                    Navigator.pop(context);
+                                  }
+                                }
+                              }),
+                              child: TextWidget(
+                                text: 'Continue',
+                                fontSize: 14,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        );
+                      }),
+                    );
+                  },
+                  child: TextWidget(
+                    text: 'Forgot Password?',
+                    fontSize: 12,
+                    color: blue,
+                    fontFamily: 'Medium',
+                  ),
+                ),
+              ),
             ),
             const SizedBox(
               height: 30,
