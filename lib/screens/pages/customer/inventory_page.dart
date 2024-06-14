ef7c3619201942d76605be1d200d75d392c7b627
Lans12345 <olanalans12345@gmail.com>
2024-06-14 08:02:22 +0800

    fixed bayanihan fund

--- lib/screens/pages/customer/inventory_page.dart
+++ lib/screens/pages/customer/inventory_page.dart
@@ -15,6 +15,8 @@ class CustomerInventoryPage extends StatefulWidget {
 class _CustomerInventoryPageState extends State<CustomerInventoryPage> {
   final searchController = TextEditingController();
   String nameSearched = '';
+
+  int position = 0;
   @override
   Widget build(BuildContext context) {
     final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
@@ -62,8 +64,7 @@ class _CustomerInventoryPageState extends State<CustomerInventoryPage> {
                     ),
                     Center(
                       child: TextWidget(
-                        text: double.parse((mydata['pts'] / 50).toString())
-                            .toStringAsFixed(0),
+                        text: position.toString(),
                         fontFamily: 'Bold',
                         fontSize: 75,
                         color: Colors.white,
@@ -73,8 +74,7 @@ class _CustomerInventoryPageState extends State<CustomerInventoryPage> {
                       child: Column(
                         children: [
                           TextWidget(
-                            text:
-                                '${double.parse((mydata['pts'] / 50).toString()).toStringAsFixed(0)} slots',
+                            text: 'Current Slot',
                             fontSize: 18,
                             color: Colors.white,
                             fontFamily: 'Bold',
@@ -94,7 +94,7 @@ class _CustomerInventoryPageState extends State<CustomerInventoryPage> {
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               TextWidget(
-                                text: 'Your Slots',
+                                text: 'Slot Progress',
                                 fontSize: 18,
                                 color: Colors.white,
                                 fontFamily: 'Bold',
@@ -144,10 +144,94 @@ class _CustomerInventoryPageState extends State<CustomerInventoryPage> {
                           //     color: Colors.grey,
                           //   ),
                           // ),
+                          // StreamBuilder<QuerySnapshot>(
+                          //     stream: FirebaseFirestore.instance
+                          //         .collection('Users')
+                          //         .where('pts', isGreaterThanOrEqualTo: 50)
+                          //         .snapshots(),
+                          //     builder: (BuildContext context,
+                          //         AsyncSnapshot<QuerySnapshot> snapshot) {
+                          //       if (snapshot.hasError) {
+                          //         print(snapshot.error);
+                          //         return const Center(child: Text('Error'));
+                          //       }
+                          //       if (snapshot.connectionState ==
+                          //           ConnectionState.waiting) {
+                          //         return const Padding(
+                          //           padding: EdgeInsets.only(top: 50),
+                          //           child: Center(
+                          //               child: CircularProgressIndicator(
+                          //             color: Colors.black,
+                          //           )),
+                          //         );
+                          //       }
+
+                          //       final data = snapshot.requireData;
+                          //       return Row(
+                          //         mainAxisAlignment:
+                          //             MainAxisAlignment.spaceEvenly,
+                          //         children: [
+                          //           for (int i = 0; i < data.docs.length; i++)
+                          //             Stack(
+                          //               children: [
+                          //                 Column(
+                          //                   mainAxisAlignment:
+                          //                       MainAxisAlignment.center,
+                          //                   crossAxisAlignment:
+                          //                       CrossAxisAlignment.center,
+                          //                   children: [
+                          //                     CircleAvatar(
+                          //                       maxRadius: 40,
+                          //                       minRadius: 40,
+                          //                       backgroundImage: NetworkImage(
+                          //                           data.docs[i]['pic']),
+                          //                       child: Padding(
+                          //                         padding:
+                          //                             const EdgeInsets.only(
+                          //                                 top: 20),
+                          //                         child: Align(
+                          //                           alignment:
+                          //                               Alignment.bottomCenter,
+                          //                           child: Container(
+                          //                             height: 25,
+                          //                             width: 25,
+                          //                             decoration:
+                          //                                 const BoxDecoration(
+                          //                               shape: BoxShape.circle,
+                          //                               color: Colors.white,
+                          //                             ),
+                          //                             child: Center(
+                          //                               child: TextWidget(
+                          //                                 text: '${i + 1}',
+                          //                                 fontSize: 12,
+                          //                                 fontFamily: 'Bold',
+                          //                               ),
+                          //                             ),
+                          //                           ),
+                          //                         ),
+                          //                       ),
+                          //                     ),
+                          //                     const SizedBox(
+                          //                       height: 5,
+                          //                     ),
+                          //                     TextWidget(
+                          //                       text: data.docs[i]['name'],
+                          //                       fontSize: 18,
+                          //                       fontFamily: 'Bold',
+                          //                       color: Colors.white,
+                          //                     ),
+                          //                   ],
+                          //                 ),
+                          //               ],
+                          //             ),
+                          //         ],
+                          //       );
+                          //     }),
+
                           StreamBuilder<QuerySnapshot>(
                               stream: FirebaseFirestore.instance
                                   .collection('Users')
-                                  .where('pts', isGreaterThanOrEqualTo: 50)
+                                  .orderBy('pts')
                                   .snapshots(),
                               builder: (BuildContext context,
                                   AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -167,122 +251,83 @@ class _CustomerInventoryPageState extends State<CustomerInventoryPage> {
                                 }
 
                                 final data = snapshot.requireData;
-                                return Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    for (int i = 0; i < data.docs.length; i++)
-                                      Stack(
-                                        children: [
-                                          Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              CircleAvatar(
-                                                maxRadius: 40,
-                                                minRadius: 40,
-                                                backgroundImage: NetworkImage(
-                                                    data.docs[i]['pic']),
-                                                child: Padding(
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          top: 20),
-                                                  child: Align(
-                                                    alignment:
-                                                        Alignment.bottomCenter,
-                                                    child: Container(
-                                                      height: 25,
-                                                      width: 25,
-                                                      decoration:
-                                                          const BoxDecoration(
-                                                        shape: BoxShape.circle,
-                                                        color: Colors.white,
-                                                      ),
-                                                      child: Center(
-                                                        child: TextWidget(
-                                                          text: '${i + 1}',
-                                                          fontSize: 12,
-                                                          fontFamily: 'Bold',
-                                                        ),
-                                                      ),
-                                                    ),
+                                return SizedBox(
+                                  height: 300,
+                                  child: ListView.builder(
+                                    itemCount: data.docs.length,
+                                    itemBuilder: (context, index) {
+                                      if (data.docs[index].id ==
+                                          FirebaseAuth
+                                              .instance.currentUser!.uid) {
+                                        WidgetsBinding.instance
+                                            .addPostFrameCallback((timeStamp) {
+                                          position = index + 1;
+                                        });
+                                      }
+                                      return Padding(
+                                        padding: const EdgeInsets.all(5.0),
+                                        child: ListTile(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                              15,
+                                            ),
+                                          ),
+                                          tileColor: Colors.white,
+                                          leading: SizedBox(
+                                            height: 50,
+                                            width: 300,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              children: [
+                                                TextWidget(
+                                                  text: '${index + 1}',
+                                                  fontSize: 11,
+                                                  color: Colors.black,
+                                                  fontFamily: 'Bold',
+                                                ),
+                                                const SizedBox(
+                                                  width: 20,
+                                                ),
+                                                CircleAvatar(
+                                                  maxRadius: 20,
+                                                  minRadius: 20,
+                                                  backgroundImage: NetworkImage(
+                                                      data.docs[index]['pic']),
+                                                ),
+                                                const SizedBox(
+                                                  width: 20,
+                                                ),
+                                                TextWidget(
+                                                  text: data.docs[index]
+                                                      ['name'],
+                                                  fontSize: 16,
+                                                  color: Colors.black,
+                                                  fontFamily: 'Bold',
+                                                ),
+                                                const Expanded(
+                                                  child: SizedBox(
+                                                    width: 20,
                                                   ),
                                                 ),
-                                              ),
-                                              const SizedBox(
-                                                height: 5,
-                                              ),
-                                              TextWidget(
-                                                text: data.docs[i]['name'],
-                                                fontSize: 18,
-                                                fontFamily: 'Bold',
-                                                color: Colors.white,
-                                              ),
-                                            ],
+                                                TextWidget(
+                                                  text:
+                                                      '${data.docs[index]['pts']}pts',
+                                                  fontSize: 14,
+                                                  color: secondary,
+                                                  fontFamily: 'Bold',
+                                                ),
+                                              ],
+                                            ),
                                           ),
-                                        ],
-                                      ),
-                                  ],
-                                );
-                              }),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          SizedBox(
-                            height: 300,
-                            child: ListView.builder(
-                              itemCount: 0,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(5.0),
-                                  child: ListTile(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                        15,
-                                      ),
-                                    ),
-                                    tileColor: Colors.white,
-                                    leading: SizedBox(
-                                      height: 50,
-                                      width: 300,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          TextWidget(
-                                            text: '${index + 1}',
-                                            fontSize: 11,
-                                            color: Colors.black,
-                                            fontFamily: 'Bold',
-                                          ),
-                                          const SizedBox(
-                                            width: 20,
-                                          ),
-                                          const CircleAvatar(
-                                            maxRadius: 25,
-                                            minRadius: 25,
-                                          ),
-                                          const SizedBox(
-                                            width: 20,
-                                          ),
-                                          TextWidget(
-                                            text: 'John Doe',
-                                            fontSize: 16,
-                                            color: Colors.black,
-                                            fontFamily: 'Bold',
-                                          ),
-                                        ],
-                                      ),
-                                    ),
+                                        ),
+                                      );
+                                    },
                                   ),
                                 );
-                              },
-                            ),
-                          ),
+                              }),
                         ],
                       ),
                     ),
