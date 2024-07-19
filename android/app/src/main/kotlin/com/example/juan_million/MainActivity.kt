18ab30edf80d9896a55260ecf6367564d48af98d
Lans12345 <olanalans12345@gmail.com>
2024-07-19 13:46:35 +0800

    fixed qr scanning

--- android/app/src/main/kotlin/com/example/juan_million/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/juan_million/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.juan_million
 
-    import io.flutter.embedding.android.FlutterFragmentActivity
-    
-    class MainActivity: FlutterFragmentActivity() {
-    }
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
